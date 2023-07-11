4766c53adb7ad32d61629f365ce1f306a441e61d
Erandika <erandika@solistech.com>
2023-07-11 23:43:05 +0530

    feat: initial commit.

---
+++ getmain.ps1
@@ -0,0 +1,16 @@
+$source = "https://github.com/erandika1986/Dynami.IdentityServer4.UI/main.zip"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest $source -OutFile ui.zip
+
+Expand-Archive ui.zip
+
+if (!(Test-Path -Path Quickstart))  { mkdir Quickstart }
+if (!(Test-Path -Path Views))       { mkdir Views }
+if (!(Test-Path -Path wwwroot))     { mkdir wwwroot }
+
+copy .\ui\Dynami.IdentityServer4.UI-main\Quickstart\* Quickstart -recurse -force
+copy .\ui\Dynami.IdentityServer4.UI-main\Views\* Views -recurse -force
+copy .\ui\Dynami.IdentityServer4.UI-main\wwwroot\* wwwroot -recurse -force
+
+del ui.zip
+del ui -recurse
---
+++ getmain.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+SOURCE="https://github.com/erandika1986/Dynami.IdentityServer4.UI/main.zip"
+curl -L -o ui.zip "$SOURCE"
+
+unzip -d ui ui.zip
+
+[[ -d Quickstart ]] || mkdir Quickstart
+[[ -d Views ]] || mkdir Views
+[[ -d wwwroot ]] || mkdir wwwroot
+
+cp -r ./ui/Dynami.IdentityServer4.UI-main/Quickstart/* Quickstart
+cp -r ./ui/Dynami.IdentityServer4.UI-main/Views/* Views
+cp -r ./ui/Dynami.IdentityServer4.UI-main/wwwroot/* wwwroot
+
+rm -rf ui ui.zip
